c99518d1973291fdceb81328de73c086d46c8eb3
Craig Schwartz <craigschwartz@Craigs-MacBook-Pro.local>
2018-05-20 11:50:23 -0400

    Migrate to keystone 4.0.0-beta.7

--- keystone.js
+++ keystone.js
@@ -28,6 +28,8 @@ keystone.init({
 		extname: '.hbs',
 	}).engine,
 
+	'emails': 'templates/emails',
+
 	'auto update': true,
 	'session': true,
 	'auth': true,
@@ -49,11 +51,6 @@ keystone.set('locals', {
 
 // Load your project's Routes
 keystone.set('routes', require('./routes'));
-
-// Switch Keystone Email defaults to handlebars
-keystone.Email.defaults.templateExt = 'hbs';
-keystone.Email.defaults.templateEngine = require('handlebars');
-
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
